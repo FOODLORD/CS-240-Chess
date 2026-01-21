00acb43647d691980cb5eba702111770ac53f1bc
Sherwinn <sherwinn@byu.edu>
2026-01-20 22:21:36 -0700

    PawnMovesCalculator.java done with test cases pass.

--- shared/src/main/java/chess/MoveCalculator/PawnMovesCalculator.java
+++ shared/src/main/java/chess/MoveCalculator/PawnMovesCalculator.java
@@ -1,8 +1,6 @@
 package chess.MoveCalculator;
 
-import chess.ChessBoard;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,8 +11,88 @@ public class PawnMovesCalculator implements PieceMovesCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         List<ChessMove> moves = new ArrayList<>();
 
+        ChessPiece piece = board.getPiece(position);
+        ChessGame.TeamColor color = piece.getTeamColor();
+
         //Pawn moves
 
+        int move_forward;
+        int start_row;
+        int promotion_row;
+
+        if (color == ChessGame.TeamColor.WHITE) {
+            move_forward = 1;
+            start_row = 2;
+            promotion_row = 8;
+        } else { //black
+            move_forward = -1;
+            start_row = 7;
+            promotion_row = 1;
+        }
+
+        int row_position = position.getRow();
+        int col_position = position.getColumn();
+
+        //moving forward
+        int next_row = row_position + move_forward;
+        if (next_row >= 1 && next_row <= 8) {
+            ChessPosition forward_position = new ChessPosition(next_row, col_position);
+            if (board.getPiece(forward_position) == null) {
+
+                //promotion
+                if (next_row == promotion_row) {
+                    PromotionMoves(moves, position, forward_position);
+                } else {
+                    moves.add(new ChessMove(position, forward_position, null));
+                }
+
+                //move to squares from the start
+                if (row_position == start_row) {
+                    int move_double_row = row_position + 2 * move_forward;
+                    ChessPosition doublePos = new ChessPosition(move_double_row, col_position);
+                    if (board.getPiece(doublePos) == null) {
+                        moves.add(new ChessMove(position, doublePos, null));
+                    }
+                }
+            }
+        }
+
+        //capturing diagonally
+
+        int[][] capture_direction = {
+                {move_forward, -1}, //up1-left1
+                {move_forward, 1} //up1-right1
+        };
+
+        for (int[] dir : capture_direction) {
+            int target_row = row_position + dir[0];
+            int target_col = col_position + dir[1];
+
+            if (target_row < 1 || target_row > 8 || target_col < 1 || target_col > 8) {
+                continue;
+            }
+
+            ChessPosition target = new ChessPosition(target_row, target_col);
+            ChessPiece targetPiece = board.getPiece(target);
+
+            if (targetPiece != null && targetPiece.getTeamColor() != color) {
+                if (target_row == promotion_row) {
+                    PromotionMoves(moves, position, target);
+                } else {
+                    moves.add(new ChessMove(position, target, null));
+                }
+            }
+        }
+
+
         return moves;
+    }
+
+    private void PromotionMoves(List<ChessMove> moves, ChessPosition from_position, ChessPosition to_target
+    ) {
+        moves.add(new ChessMove(from_position, to_target, ChessPiece.PieceType.QUEEN));
+        moves.add(new ChessMove(from_position, to_target, ChessPiece.PieceType.ROOK));
+        moves.add(new ChessMove(from_position, to_target, ChessPiece.PieceType.BISHOP));
+        moves.add(new ChessMove(from_position, to_target, ChessPiece.PieceType.KNIGHT));
     }
 }
