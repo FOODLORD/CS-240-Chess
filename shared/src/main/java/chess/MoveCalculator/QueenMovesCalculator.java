7d4cdabc9386397054175adf24d797bd77e84dd6
Sherwinn <sherwinn@byu.edu>
2026-01-20 23:53:41 -0700

    QueenMovesCalculator.java done with test cases pass.

--- shared/src/main/java/chess/MoveCalculator/QueenMovesCalculator.java
+++ shared/src/main/java/chess/MoveCalculator/QueenMovesCalculator.java
@@ -1,8 +1,6 @@
 package chess.MoveCalculator;
 
-import chess.ChessBoard;
-import chess.ChessMove;
-import chess.ChessPosition;
+import chess.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,7 +11,47 @@ public class QueenMovesCalculator implements PieceMovesCalculator{
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition position) {
         List<ChessMove> moves = new ArrayList<>();
 
+        ChessPiece piece = board.getPiece(position);
+        ChessGame.TeamColor color = piece.getTeamColor();
+
         //Queen moves
+
+        int[][] move_direction = {
+                { 1,  0},   // move up
+                {-1,  0},   // move down
+                { 0,  1},   // move right
+                { 0, -1},   // move left
+                { 1,  1},   // move diaonally right
+                { 1, -1},   // move diagonally left
+                {-1,  1},   // move diagonally right downwards
+                {-1, -1}    // move diagonally left downwards
+        };
+
+        for (int[] dir : move_direction) {
+            int next_row = position.getRow() + dir[0];
+            int next_col = position.getColumn() + dir[1];
+
+            while (next_row >= 1 && next_row <= 8 && next_col >= 1 && next_col <= 8) {
+
+                ChessPosition target = new ChessPosition(next_row, next_col);
+                ChessPiece targetPiece = board.getPiece(target);
+
+                if (targetPiece == null) {
+                    // move to empty square
+                    moves.add(new ChessMove(position, target, null));
+                } else {
+                    // square have another piece
+                    if (targetPiece.getTeamColor() != color) {
+                        // enemy piece
+                        moves.add(new ChessMove(position, target, null));
+                    }
+                    break;
+                }
+
+                next_row += dir[0];
+                next_col += dir[1];
+            }
+        }
 
         return moves;
     }
