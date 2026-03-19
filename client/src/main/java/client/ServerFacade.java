0efa23b9a2fa95107c85402e93af4c333f611a1e
Sherwinn <sherwinn@byu.edu>
2026-03-18 23:33:04 -0600

    Added register and helper functions in server facade

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -1,4 +1,83 @@
 package client;
 
+import com.google.gson.Gson;
+import exception.ResponseException;
+import model.*;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.*;
+import java.net.http.HttpRequest.BodyPublisher;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse.BodyHandlers;
+
 public class ServerFacade {
+
+    private final String serverUrl;
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final Gson gson = new Gson();
+
+    public ServerFacade(String serverUrl) {
+        this.serverUrl = serverUrl;
+    }
+
+    //register
+    public RegisterResponse register(RegisterRequest request) throws ResponseException {
+        var httpRequest = buildRequest("POST", "/user", request, null);
+        var response = sendRequest(httpRequest);
+        return handleResponse(response, RegisterResponse.class);
+    }
+
+
+    //helper func
+
+    private HttpRequest buildRequest(String method, String path, Object body, String authToken) {
+        HttpRequest.Builder builder = HttpRequest.newBuilder()
+                .uri(URI.create(serverUrl + path))
+                .method(method, makeBody(body));
+
+        if (body != null) {
+            builder.setHeader("Content-Type", "application/json");
+        }
+
+        if (authToken != null) {
+            builder.setHeader("authorization", authToken);
+        }
+
+        return builder.build();
+    }
+
+    private BodyPublisher makeBody(Object body) {
+        if (body == null) {
+            return BodyPublishers.noBody();
+        }
+        return BodyPublishers.ofString(gson.toJson(body));
+    }
+
+    private HttpResponse<String> sendRequest(HttpRequest request) throws ResponseException {
+        try {
+            return client.send(request, BodyHandlers.ofString());
+        } catch (Exception error) {
+            throw new ResponseException(ResponseException.Code.ServerError, error.getMessage());
+        }
+    }
+
+    private <T> T handleResponse(HttpResponse<String> response, Class<T> responseClass) throws ResponseException {
+        int status = response.statusCode();
+
+        if (status / 100 != 2) {
+            var body = response.body();
+            if (body != null) {
+                throw ResponseException.fromJson(body);
+            }
+
+            throw new ResponseException(ResponseException.fromHttpStatusCode(status), "other failure: " + status);
+        }
+
+        if (responseClass != null) {
+            return gson.fromJson(response.body(), responseClass);
+        }
+
+        return null;
+    }
 }
