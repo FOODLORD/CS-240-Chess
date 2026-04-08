501cc611f054f54a9af5a1f5122e8f6067f89a05
Sherwinn <sherwinn@byu.edu>
2026-04-07 21:29:41 -0600

    Implemented makemove

--- server/src/main/java/websocket/WebSocketHandler.java
+++ server/src/main/java/websocket/WebSocketHandler.java
@@ -1,6 +1,8 @@
 package websocket;
+import chess.ChessGame;
 import dataaccess.DataAccess;
 import com.google.gson.Gson;
+import model.GameData;
 import org.eclipse.jetty.websocket.api.Session;
 
 import websocket.commands.UserGameCommand;
@@ -17,11 +19,14 @@ public class WebSocketHandler {
 
     public void onMessage(Session session, String message) throws Exception {
 
-        UserGameCommand command = new Gson().fromJson(message, UserGameCommand.class);
+        var command = new Gson().fromJson(message, UserGameCommand.class);
 
         switch (command.getCommandType()) {
             case CONNECT -> connect(session, command);
-            case MAKE_MOVE -> makeMove(session, command);
+            case MAKE_MOVE -> {
+                var moveCommand = new Gson().fromJson(message, websocket.commands.MakeMoveCommand.class);
+                makeMove(session, command);
+            }
             case LEAVE -> leave(session, command);
             case RESIGN -> resign(session, command);
         }
@@ -58,7 +63,79 @@ public class WebSocketHandler {
         );
     }
 
-    private void makeMove(Session session, UserGameCommand command) throws Exception {}
+    private void makeMove(Session session, UserGameCommand command) throws Exception {
+
+        var auth = dataAccess.getAuth(command.getAuthToken());
+        if (auth == null) {
+            throw new Exception("Error: unauthorized");
+        }
+        String username = auth.username();
+
+        int gameID = command.getGameID();
+
+
+        var gameData = dataAccess.getGame(gameID);
+        if (gameData == null || gameData.game() == null) {
+            throw new Exception("Error: bad request");
+        }
+
+        var game = gameData.game();
+
+
+        ChessGame.TeamColor playerColor;
+
+        if (username.equals(gameData.whiteUsername())) {
+            playerColor = ChessGame.TeamColor.WHITE;
+        } else if (username.equals(gameData.blackUsername())) {
+            playerColor = ChessGame.TeamColor.BLACK;
+        } else {
+            throw new Exception("Error: observer cannot make moves");
+        }
+
+
+        if (game.getTeamTurn() != playerColor) {
+            throw new Exception("Error: not player turn");
+        }
+
+
+        var moveCommand = (websocket.commands.MakeMoveCommand) command;
+        var move = moveCommand.getMove();
+
+
+        try {
+            game.makeMove(move);
+        } catch (Exception error) {
+            throw new Exception("Error: invalid move");
+        }
+
+
+        GameData updatedGame = new GameData(
+                gameID,
+                gameData.whiteUsername(),
+                gameData.blackUsername(),
+                gameData.gameName(),
+                game
+        );
+
+        dataAccess.updateGame(updatedGame);
+
+
+        connectionManager.broadcast(
+                gameID,
+                null,
+                new LoadGameMessage(game)
+        );
+
+
+        String message = username + " moved from " + move.getStartPosition() + " to " + move.getEndPosition();
+
+        connectionManager.broadcast(
+                gameID,
+                session,
+                new NotificationMessage(message)
+        );
+
+    }
 
     private void leave(Session session, UserGameCommand command) throws Exception {}
 
